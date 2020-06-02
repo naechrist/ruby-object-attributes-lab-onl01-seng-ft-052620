28ce14f2a524f67b5c526044461df787e6fe6b86
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-02 17:25:13 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
 class Person
   
+  def name=(persons_name)
+    @name = persons_name
+  end
+  
+  def name 
+    @name 
+  end
 end
+
+beyonce = Person.new 
+beyonce.name = "Beyonce"
+beyonce.name 
